93ceec9df27724d335facb856cc14e259fcf0cae
MXAsan <xasannet@icloud.com>
2026-04-22 18:33:38 +0500

    add languge

---
+++ src/leftSide/leftSideLanguage/leftSideLanguage.tsx
@@ -0,0 +1,31 @@
+const languages = [
+  {
+    text: "Uzbek",
+    image: "uz.svg",
+  },
+  {
+    text: "English",
+    image: "eng.svg",
+  },
+  {
+    text: "Russian",
+    image: "ru.svg",
+  },
+];
+const LeftSideLanguage = () => {
+  return (
+    <div>
+      <p className="mini-titles">LANGUAGES</p>
+      <div className="skills-container">
+        {languages.map((language, index) => (
+          <div className="skills" key={index}>
+            <img src={language.image} alt="" />
+            <p>{language.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default LeftSideLanguage;
