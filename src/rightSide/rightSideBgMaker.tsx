a595820ca97c828367eb1f34aedce5ae4821253b
MXAsan <xasannet@icloud.com>
2026-05-12 14:02:29 +0500

    change the function to show a black bg

--- src/rightSide/rightSideBgMaker.tsx
+++ src/rightSide/rightSideBgMaker.tsx
@@ -3,9 +3,13 @@ import { useEffect } from "react";
 const RightSideBgMaker = () => {
   useEffect(() => {
     const rightSide = document.querySelector(".right"),
-      randomNum = Math.floor(Math.random() * 3) + 1;
-    if (rightSide) {
-      rightSide.style.backgroundImage = `url('bg/bg-${randomNum}.jpeg')`;
+      randomNum = Math.floor(Math.random() * 4) + 1;
+    if (randomNum >= 3) {
+      rightSide.style.backgroundImage = "black";
+    } else {
+      if (rightSide) {
+        rightSide.style.backgroundImage = `url('bg/bg-${randomNum}.jpeg')`;
+      }
     }
   }, []);
 };
