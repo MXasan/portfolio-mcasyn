eccc75dbb9683e26527e8f468b8e8bd21d15ebde
MXAsan <xasannet@icloud.com>
2026-05-13 19:06:02 +0500

    fix: bugs with bg makker

--- src/rightSide/rightSideBgMaker.tsx
+++ src/rightSide/rightSideBgMaker.tsx
@@ -2,14 +2,12 @@ import { useEffect } from "react";
 
 const RightSideBgMaker = () => {
   useEffect(() => {
-    const rightSide = document.querySelector(".right"),
-      randomNum = Math.floor(Math.random() * 5) + 1;
+    const rightSide = document.querySelector<HTMLElement>(".right");
+    const randomNum = Math.floor(Math.random() * 5) + 1;
     if (randomNum >= 6) {
-      rightSide.style.backgroundImage = "black";
+      if (rightSide) rightSide.style.backgroundImage = "black";
     } else {
-      if (rightSide) {
-        rightSide.style.backgroundImage = `url('bg/bg-${randomNum}.jpg')`;
-      }
+      if (rightSide) rightSide.style.backgroundImage = `url('bg/bg-${randomNum}.jpg')`;
     }
   }, []);
 };
