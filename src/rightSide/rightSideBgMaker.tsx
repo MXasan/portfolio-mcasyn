8fb38c02f3692295c6adab5090a32e742eb23c7c
MXAsan <xasannet@icloud.com>
2026-05-14 13:54:34 +0500

    change the chanse of bg

--- src/rightSide/rightSideBgMaker.tsx
+++ src/rightSide/rightSideBgMaker.tsx
@@ -4,7 +4,7 @@ const RightSideBgMaker = () => {
   useEffect(() => {
     const rightSide = document.querySelector<HTMLElement>(".bg-fixed");
     const randomNum = Math.floor(Math.random() * 4) + 1;
-    if (randomNum >= 3) {
+    if (randomNum >= 5) {
       if (rightSide) rightSide.style.backgroundImage = "black";
     } else {
       if (rightSide) rightSide.style.backgroundImage = `url('bg/bg-${randomNum}.jpg')`;
